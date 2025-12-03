a7902df3caa38647f702dd0bf7e6e9d8f145491a
Mosaab Alsaed <m.alseed@tawasulforum.org>
2025-12-03 15:18:23 +0300

    simple fix of {}

--- inc/wpbakery-widgets/call-shortcods.php
+++ inc/wpbakery-widgets/call-shortcods.php
@@ -13,6 +13,6 @@ add_action('vc_before_init', function () {
     require_once get_template_directory() . '/inc/wpbakery-widgets/vcmaps/about-us-map.php';
     require_once get_template_directory() . '/inc/wpbakery-widgets/vcmaps/hero-section-map.php';
     require_once get_template_directory() . '/inc/wpbakery-widgets/vcmaps/our-values-map.php';
-    require_once get_template_directory() . '/inc/wpbakery-widgets/vcmaps/st
+    require_once get_template_directory() . '/inc/wpbakery-widgets/vcmaps/statistics-map.php';
     require_once get_template_directory() . '/inc/wpbakery-widgets/vcmaps/contact-section-map.php';
 });
