a47528093f653c06d0d4dc5cd8474c1fe325b537
Mosaab Alsaed <m.alseed@tawasulforum.org>
2026-01-12 16:46:02 +0300

    Add styling to wpbackry html block

--- inc/wpbakery-widgets/shortcodes/html-block-view.php
+++ inc/wpbakery-widgets/shortcodes/html-block-view.php
@@ -17,11 +17,19 @@ if (!function_exists('html_block_shortcode')) {
         if (!$post) {
             return '';
         }
+        // Source - https://stackoverflow.com/a
+        // Posted by shagshag
+        // Retrieved 2026-01-12, License - CC BY-SA 4.0
+
+
         // make sure it's the correct post type
         if ($post->post_type !== 'html_blocks') return '';
         $block_content =  $post->post_content;
         ob_start();
 ?>
+        <style type="text/css" data-type="vc_shortcodes-custom-css">
+            <?php echo get_post_meta($block_id, '_wpb_shortcodes_custom_css', true); ?>
+        </style>
         <section class="normal-margin">
             <div class="container">
                 <?php echo apply_filters('the_content', $block_content); ?>
